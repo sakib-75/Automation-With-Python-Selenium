73a4ce39ff2b57b1fb9a38885814779f14c5b907
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2022-04-30 17:54:59 +0600

    Update test_rsa.py

--- Tests/Tests_2/test_rsa.py
+++ Tests/Tests_2/test_rsa.py
@@ -7,9 +7,17 @@ from Pages.Pages_2.rsa_home_page import RsaHomePage
 class TestRsa:
     def test_tc01(self):
         self.driver.get("https://rahulshettyacademy.com/locatorspractice")
-
         home = RsaHomePage(self.driver)
         home.click_signin_button()
         expected_error_msg = "* Incorrect username or password"
-
         assert expected_error_msg == home.get_login_error_msg()
+
+    def test_tc02(self):
+        self.driver.refresh()
+        home = RsaHomePage(self.driver)
+        home.click_check_box1()
+        home.click_check_box2()
+        home.click_signin_button()
+        expected_error_msg = "* Incorrect username or password"
+        assert expected_error_msg == home.get_login_error_msg()
+
