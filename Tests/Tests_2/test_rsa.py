98671591d662ff5b570eb9273329a64e76b49ecd
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2022-05-01 12:02:33 +0600

    Update test_rsa.py

--- Tests/Tests_2/test_rsa.py
+++ Tests/Tests_2/test_rsa.py
@@ -5,12 +5,13 @@ from Pages.Pages_2.rsa_home_page import RsaHomePage
 
 @pytest.mark.usefixtures("setup")
 class TestRsa:
+    expected_error_msg = "* Incorrect username or password"
+
     def test_all_empty_input(self):
         self.driver.get("https://rahulshettyacademy.com/locatorspractice")
         home = RsaHomePage(self.driver)
         home.click_signin_button()
-        expected_error_msg = "* Incorrect username or password"
-        assert expected_error_msg == home.get_login_error_msg()
+        assert self.expected_error_msg == home.get_login_error_msg()
 
     def test_empty_input(self):
         self.driver.refresh()
@@ -18,12 +19,10 @@ class TestRsa:
         home.click_check_box1()
         home.click_check_box2()
         home.click_signin_button()
-        expected_error_msg = "* Incorrect username or password"
-        assert expected_error_msg == home.get_login_error_msg()
+        assert self.expected_error_msg == home.get_login_error_msg()
 
     def test_signup_wrong(self):
         self.driver.refresh()
         home = RsaHomePage(self.driver)
         home.login("sakib", "123")
-        expected_error_msg = "* Incorrect username or password"
-        assert expected_error_msg == home.get_login_error_msg()
+        assert self.expected_error_msg == home.get_login_error_msg()
