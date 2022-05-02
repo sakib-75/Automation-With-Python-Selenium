4989be7d45d976af2ae0c886e9465de6811979a2
Sakibul Islam <61193926+sakib-75@users.noreply.github.com>
2022-05-02 10:53:53 +0600

    Create select_drop_down.py

---
+++ Tests/Tests_1/select_drop_down.py
@@ -0,0 +1,37 @@
+import time
+
+import pytest
+from selenium.webdriver import ActionChains, Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+
+@pytest.mark.usefixtures("setup")
+class TestSelectDropDown:
+    def test_select_drop_down(self):
+        driver = self.driver
+        driver.get("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select_multiple")
+        driver.switch_to.frame("iframeResult")
+
+        select_element = driver.find_element(By.NAME, "cars")
+        select = Select(select_element)
+        select.select_by_index(0)
+
+        actions = ActionChains(driver)
+        actions.key_down(Keys.CONTROL) \
+            .click(select.options[3]) \
+            .click(select.options[1]) \
+            .key_up(Keys.CONTROL).perform()
+
+        driver.switch_to.default_content()
+        time.sleep(2)
+
+        # select.select_by_visible_text("text")
+        # select.select_by_value(value)
+        # select.deselect_all()
+        # select.deselect_by_index()
+        # select.deselect_by_visible_text()
+        # select.deselect_by_value()
+        # select.all_selected_options
+        # select.first_selected_option
+        # select.options
